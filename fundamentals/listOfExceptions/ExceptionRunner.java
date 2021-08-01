a35c16ed32af1054ca781b9b81926cbcad0f7fb4
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-08-01 11:45:32 -0700

    Error

---
+++ fundamentals/listOfExceptions/ExceptionRunner.java
@@ -0,0 +1,18 @@
+public class ExceptionRunner{
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        try {
+           for(int i = 0; i < myList.size(); i++) {
+            Integer castedValue = (Integer) myList.get(i);
+            } 
+        } catch (ClassCastException error){
+            System.out.println(error);
+        } finally {
+            System.out.println("Completed.");
+        }
+    }
+}
