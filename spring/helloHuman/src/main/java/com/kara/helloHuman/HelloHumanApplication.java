836b08e632e13c6f28f40a8961feeb0490bea3d7
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-08-02 15:25:59 -0700

    HelloHuman

--- spring/helloHuman/src/main/java/com/kara/helloHuman/HelloHumanApplication.java
+++ spring/helloHuman/src/main/java/com/kara/helloHuman/HelloHumanApplication.java
@@ -2,12 +2,32 @@ package com.kara.helloHuman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//2. Importing dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+//1. Annotation
+@RestController
+@RequestMapping("/")
 public class HelloHumanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloHumanApplication.class, args);
 	}
+	
+	// 1. Annotation
+    @RequestMapping("")
+    // 3. Method that maps to the request route above
+    public String hello() { 
+    	String returnThis = "Hello Human!" + '\n' + "Welcome To Spring Boot!";
+    	return returnThis;
+    }
+    
+    @RequestMapping("{name}")
+    public String namesDisplayed(@PathVariable("name") String name) {
+    	String returnString = "Hello " + name +  "! Welcome to SpringBoot!";
+    	return returnString;
+    }
 
 }
