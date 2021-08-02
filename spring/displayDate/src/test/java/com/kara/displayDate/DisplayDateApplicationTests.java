6ec3dbdf4b729b5adf29e55bdda9220bee38ae37
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-08-02 15:40:23 -0700

    initialize datetime

---
+++ spring/displayDate/src/test/java/com/kara/displayDate/DisplayDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kara.displayDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
